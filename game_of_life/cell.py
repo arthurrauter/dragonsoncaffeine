94c4723955b7aba20d332aa58b54e6038f3d1ca1
Arthur Rauter <arthur@brightfor.me>
2016-07-08 09:36:05 +0200

    :set expandtab & :retab applied

--- game_of_life/cell.py
+++ game_of_life/cell.py
@@ -1,24 +1,24 @@
 class Cell:
-	def __init__(self, state, neighbours):
-		self.state = state
-		self.neighbours = neighbours
+    def __init__(self, state, neighbours):
+        self.state = state
+        self.neighbours = neighbours
 
-	def regenerate(self):
-		#count live neighbours
-		live_neighbours = 0
-		for cell in self.neighbours:
-			if cell == 1:
-				live_neighbours += 1
-		
-		if self.state == 'live':
-			if live_neighbours < 2:
-				self.state = 'dead'
-				#print "so lonely..."
-			if live_neighbours > 3:
-				self.state = 'dead'
-				#print "overcrowded!"
-		else:
-			if live_neighbours == 3:
-				self.state = 'live'
-				#print "the miracle of life"
+    def regenerate(self):
+        #count live neighbours
+        live_neighbours = 0
+        for cell in self.neighbours:
+            if cell == 1:
+                live_neighbours += 1
+        
+        if self.state == 'live':
+            if live_neighbours < 2:
+                self.state = 'dead'
+                #print "so lonely..."
+            if live_neighbours > 3:
+                self.state = 'dead'
+                #print "overcrowded!"
+        else:
+            if live_neighbours == 3:
+                self.state = 'live'
+                #print "the miracle of life"
 
